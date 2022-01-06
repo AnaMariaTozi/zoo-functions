dfac016120df5a3a5da4df27a36fbf44a2d971ef
Ana Maria Tozi <anamariatozi@gmail.com>
2022-01-06 16:57:14 -0300

    Requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,10 +1,17 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
+  //const {species} = data;
   // seu código aqui
   if(!ids){
-    return undefined
-  }
-}
+    const empty = []
+    return empty
+  } 
 
+  //const espécies = species.find(specie => specie.id === ids) retorna só a primeira posição
+  const espécies = ids.map((id) => species.find((specie) => specie.id === id));
+  return espécies
+}
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'))
 module.exports = getSpeciesByIds;
