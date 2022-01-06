2876151fa9ce2f05c2468a66a20172f7bdc7e5b7
Ana Maria Tozi <anamariatozi@gmail.com>
2022-01-06 18:44:10 -0300

    Requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,15 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (!employeeName) {
+    const empty = {};
+    return empty;
+  };
+
+  const funcionarios = employees.find(employee => employee.firstName === employeeName || employee.lastName === employeeName);
+  return funcionarios
 }
 
 module.exports = getEmployeeByName;
