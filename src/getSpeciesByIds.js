7e1bb741f6eb16661e11cd5c2dec4f5619ae6eed
Ana Maria Tozi <anamariatozi@gmail.com>
2022-01-06 17:01:32 -0300

    Arruma lint

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,16 +2,15 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
-  //const {species} = data;
   // seu código aqui
-  if(!ids){
-    const empty = []
-    return empty
-  } 
+  if (!ids) {
+    const empty = [];
+    return empty;
+  }
 
-  //const espécies = species.find(specie => specie.id === ids) retorna só a primeira posição
+  // const espécies = species.find(specie => specie.id === ids) retorna só a primeira posição
   const espécies = ids.map((id) => species.find((specie) => specie.id === id));
-  return espécies
+  return espécies;
 }
-console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'))
+
 module.exports = getSpeciesByIds;
