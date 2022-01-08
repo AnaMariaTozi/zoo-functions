5b88a4268a5c335cfbde275e81383f6c7ffd3f90
Ana Maria Tozi <anamariatozi@gmail.com>
2022-01-08 18:13:21 -0300

    Requisito 2 - getAnimalsOlderThan

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,13 +1,16 @@
-// const { species } = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
   // buscar se todos os animais da especie 'animal'
-  // buscar as idades dos animais dessa especie
   // comparar se as idades são maiores que 'age'
-  // retorna true or false
-  // desetruturar para pegar as informações de idade
-  // usa every para ver se todos os animais sao mais velhos do que a idade minima
+  // retorna true ou false
+  
+  const animals = species.find((specie) => specie.name === animal);
+  
+  const haveMinimalAge = animals.residents.every((resident) => resident.age > age);
+
+  return haveMinimalAge;
 }
 
 module.exports = getAnimalsOlderThan;
