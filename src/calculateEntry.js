480fd871de69f048a86e0d53396fd7cc1d9a2726
Ana Maria Tozi <anamariatozi@gmail.com>
2022-01-08 19:22:53 -0300

    Requisito 6 - calculateEntry

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,29 @@
-const data = require('../data/zoo_data');
+const { prices } = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const counter = { child: 0, adult: 0, senior: 0 };
+
+  entrants.forEach(({ age }) => {
+    if (age < 18) counter.child += 1;
+    if (age >= 18 && age < 50) counter.adult += 1;
+    if (age >= 50) counter.senior += 1;
+  });
+  return counter;
 }
 
 function calculateEntry(entrants) {
   // seu código aqui
+  if (!entrants || entrants.length === undefined) return 0;
+
+  const visitants = countEntrants(entrants);
+
+  const childTotal = visitants.child * prices.child;
+  const adultTotal = visitants.adult * prices.adult;
+  const seniorTotal = visitants.senior * prices.senior;
+
+  const total = childTotal + adultTotal + seniorTotal;
+
+  return total;
 }
 
 module.exports = { calculateEntry, countEntrants };
