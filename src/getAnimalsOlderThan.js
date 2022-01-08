6bbff461f1e60e96a8d4e0aa4fde9341e3112fba
Ana Maria Tozi <anamariatozi@gmail.com>
2022-01-08 19:29:55 -0300

    Arruma lint

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,12 +2,12 @@ const { species } = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
-  // buscar se todos os animais da especie 'animal'
+  // buscar se todos os animais são da especie 'animal'
   // comparar se as idades são maiores que 'age'
   // retorna true ou false
-  
+
   const animals = species.find((specie) => specie.name === animal);
-  
+
   const haveMinimalAge = animals.residents.every((resident) => resident.age > age);
 
   return haveMinimalAge;
