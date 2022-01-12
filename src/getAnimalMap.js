1528c2c9350dd16c379469e0b5e55a3fc91a99e6
Ana Maria Tozi <anamariatozi@gmail.com>
2022-01-11 21:00:13 -0300

    Requisito 7 - Parcial

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,40 @@
-const data = require('../data/zoo_data');
+/*  A função é responsável pelo mapeamento geográfico das espécies e seus animais, podendo ainda filtrá-los por ordem alfabética e sexo.
+
+  Observações técnicas
+
+  Analise o teste unitário para entender os retornos que são esperados para esta função.
+
+  O que será avaliado
+
+  - Sem parâmetros, retorna animais categorizados por localização;
+  - Sem a opção includeNames especificada, retorna animais categorizados por localização;
+  - Com a opção includeNames: true especificada, retorna nomes de animais;
+  - Com a opção sorted: true especificada, retorna nomes de animais ordenados;
+  - Com a opção sex: 'female' ou sex: 'male' especificada, retorna somente nomes de animais macho/fêmea;
+  - Com a opção sex: 'female' ou sex: 'male' especificada e a opção sorted: true especificada, retorna somente nomes de animais macho/fêmea com os nomes dos animais ordenados; */
+
+const { species } = require('../data/zoo_data');
 
 function getAnimalMap(options) {
   // seu código aqui
+
+  function byLocation() {
+    const locationObj = species.reduce((regiao, element) => {
+      if (regiao[element.location]) {
+        regiao[element.location].push(element.name);
+        return regiao;
+      }
+      regiao[element.location] = [element.name];
+      return regiao;
+    }, {});
+    //  console.log('Esse é o objeto de retorno:',locationObj)
+    return locationObj;
+  }
+
+  if (!options) {
+    console.log(byLocation());
+  }
 }
 
+getAnimalMap();
 module.exports = getAnimalMap;
