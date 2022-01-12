232061f6d8bc75493344eaca89e089c97e658a11
Ana Maria Tozi <anamariatozi@gmail.com>
2022-01-12 12:20:41 -0300

    Requisito 7 - correcao e lint

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -6,35 +6,31 @@
 
   O que será avaliado
 
-  - Sem parâmetros, retorna animais categorizados por localização;
-  - Sem a opção includeNames especificada, retorna animais categorizados por localização;
-  - Com a opção includeNames: true especificada, retorna nomes de animais;
-  - Com a opção sorted: true especificada, retorna nomes de animais ordenados;
-  - Com a opção sex: 'female' ou sex: 'male' especificada, retorna somente nomes de animais macho/fêmea;
-  - Com a opção sex: 'female' ou sex: 'male' especificada e a opção sorted: true especificada, retorna somente nomes de animais macho/fêmea com os nomes dos animais ordenados; */
+  - (x)Sem parâmetros, retorna animais categorizados por localização;
+  - ()Sem a opção includeNames especificada, retorna animais categorizados por localização;
+  - ()Com a opção includeNames: true especificada, retorna nomes de animais;
+  - ()Com a opção sorted: true especificada, retorna nomes de animais ordenados;
+  - ()Com a opção sex: 'female' ou sex: 'male' especificada, retorna somente nomes de animais macho/fêmea;
+  - ()Com a opção sex: 'female' ou sex: 'male' especificada e a opção sorted: true especificada, retorna somente nomes de animais macho/fêmea com os nomes dos animais ordenados; */
 
 const { species } = require('../data/zoo_data');
 
+// Cria o objeto dinamicamente, para retornar na função getAnimalMap se caso não tiver parametro
+const locationObj = species.reduce((acc, element) => {
+  if (!acc[element.location]) {
+    acc[element.location] = [];
+  }
+  acc[element.location].push(element.name);
+  return acc;
+}, {});
+
 function getAnimalMap(options) {
   // seu código aqui
-
-  function byLocation() {
-    const locationObj = species.reduce((regiao, element) => {
-      if (regiao[element.location]) {
-        regiao[element.location].push(element.name);
-        return regiao;
-      }
-      regiao[element.location] = [element.name];
-      return regiao;
-    }, {});
-    //  console.log('Esse é o objeto de retorno:',locationObj)
-    return locationObj;
-  }
-
   if (!options) {
-    console.log(byLocation());
+    return locationObj;
   }
 }
 
-getAnimalMap();
+// COMO RETORNAR A FUNÇÃO DENTRO DO IF???
+// console.log(getAnimalMap());
 module.exports = getAnimalMap;
