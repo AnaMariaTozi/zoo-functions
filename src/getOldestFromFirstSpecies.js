12a0cdd1f3f15a4b6447ee4ddfe57860f46e5caf
Ana Maria Tozi <anamariatozi@gmail.com>
2022-01-12 12:19:12 -0300

    Requisito 9 - tira console.log e arruma lint

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,14 +1,7 @@
 const { employees, species } = require('../data/zoo_data');
-/* A função busca por informações do animal mais velho da primeira espécie gerenciada pela pessoa colaboradora do parâmetro.
-
-O que será avaliado
-
-Passado o id de um funcionário, encontra a primeira espécie de animal gerenciado pelo funcionário, e retorna um array com nome, sexo e idade do animal mais velho dessa espécie. */
-
-// RETORNO ['Maxwell', 'male', 15]
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+// seu código aqui
   // encontra o funcionário chamado no parâmetro
   const findEmployee = employees.find((employee) => employee.id === id);
 
@@ -16,11 +9,9 @@ function getOldestFromFirstSpecies(id) {
   const firstSpecie = findEmployee.responsibleFor[0];
 
   const findSpecie = species.find((animal) => animal.id === firstSpecie);
-  //console.log(findSpecie);
 
   // encontrar qual é o mais velho entre a especie especificada acima
-  const oldest = findSpecie.residents.sort((a,b) => b.age - a.age)[0];
-  //const imprime = `${oldest.name} ${oldest.sex} ${oldest.age}`;
+  const oldest = findSpecie.residents.sort((a, b) => b.age - a.age)[0];
   const retorno = [oldest.name, oldest.sex, oldest.age];
   return retorno;
 }
